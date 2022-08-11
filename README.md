966fe55e66801d597cc34ebb551a45e2184b871c
yiPian <285673819@qq.com>
2022-08-12 00:17:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# uni-app开发钉钉小程序 引入的F2组件
+
+是将[my-f2](https://github.com/antvis/my-f2)内的逻辑提取出来，组装成uni-app里面的自定义组件。
+
+## 如何使用
+
+### 1.将此项目文件放置到根目录 *mycomponents* 文件下
+
+|- mycomponents
+    |- f2-canvas-to-dd
+        |- lib
+        |- f2-canvas.acss
+        |- f2-canvas.axml
+        |- f2-canvas.js
+        |- f2-canvas.json
+
+### 2.使用组件
+
+```vue
+<template>
+    <view>
+        <f2-canvas :on-init="onInitChart" :width="300" :height="300"></f2-canvas>
+    </view>
+</template>
+
+<script>
+    import F2Canvas from '@/mycomponents/f2-canvas-to-dd/f2-canvas'
+
+    export default {
+        components: { F2Canvas },
+        methods: {  
+            onInitChart(F2, config) {
+                const chart = new F2.Chart(config);
+                const data = [
+                { value: 63.4, city: 'New York', date: '2011-10-01' },
+                { value: 62.7, city: 'Alaska', date: '2011-10-01' },
+                { value: 72.2, city: 'Austin', date: '2011-10-01' },
+                { value: 58, city: 'New York', date: '2011-10-02' },
+                { value: 59.9, city: 'Alaska', date: '2011-10-02' },
+                { value: 67.7, city: 'Austin', date: '2011-10-02' },
+                { value: 53.3, city: 'New York', date: '2011-10-03' },
+                { value: 59.1, city: 'Alaska', date: '2011-10-03' },
+                { value: 69.4, city: 'Austin', date: '2011-10-03' },
+                ];
+                chart.source(data, {
+                date: {
+                    range: [0, 1],
+                    type: 'timeCat',
+                    mask: 'MM-DD'
+                },
+                value: {
+                    max: 300,
+                    tickCount: 4
+                }
+                });
+                chart.area().position('date*value').color('city').adjust('stack');
+                chart.line().position('date*value').color('city').adjust('stack');
+                chart.render();
+                // 注意：需要把chart return 出来
+                return chart;
+            }
+        }
+    }
+</script>
+```
+
+### 注意不要使用json文件引入组件，会报错。
+
+```json
+{
+  "usingComponents": {
+    "F2Canvas": "/mycomponents/f2-canvas-to-dd/f2-canvas"
+  }
+}
+```
---
+++ f2-canvas.js
@@ -0,0 +1,90 @@
+import F2 from './lib/f2';
+import { my as my$1 } from './lib/f2-context';
+
+function wrapEvent(e) {
+  if (!e) return;
+  if (!e.preventDefault) {
+    e.preventDefault = function() {};
+  }
+  return e;
+}
+
+Component({
+  // mixins: [],
+  // data: {},
+  props: {
+    onInit: () => {},
+    // width height 会作为元素兜底的宽高使用
+    width: null,
+    height: null
+  },
+  didMount() {
+    const pageId = this.$page && this.$page.$id || 0;
+    const id = `f2-canvas-${pageId}-${this.$id}`;
+    const myCtx = my.createCanvasContext(id, { page: this.$page });
+    const context = my$1(myCtx);
+    this.setData({
+      $pageId: pageId
+    });
+    const query = my.createSelectorQuery({ page: this.$page });
+    query
+      .select(`#${id}`)
+      .boundingClientRect()
+      .exec(res => {
+        // 获取画布实际宽高, 用props的宽高做失败兜底
+        const { width, height } = res && res[0] ? res[0] : this.props;
+        if (!width || !height) {
+          return;
+        }
+        const pixelRatio = my.getSystemInfoSync().pixelRatio;
+        // 高清解决方案
+        this.setData({
+          id,
+          width: width * pixelRatio,
+          height: height * pixelRatio
+        }, () => {
+          const ref = this;
+          const chart = this.props.onInit(F2, { context, width, height, pixelRatio }, ref);
+          if (chart) {
+            this.chart = chart;
+            this.canvasEl = chart.get('el');
+          }
+        });
+      });
+  },
+  // didUpdate() {},
+  // didUnmount() {},
+  methods: {
+    click(e) {
+      const canvasEl = this.canvasEl;
+      if (!canvasEl) {
+        return;
+      }
+      const event = wrapEvent(e);
+      // 包装成 touch 对象
+      event.touches = [ e.detail ];
+      canvasEl.dispatchEvent('click', event);
+    },
+    touchStart(e) {
+      const canvasEl = this.canvasEl;
+      if (!canvasEl) {
+        return;
+      }
+      canvasEl.dispatchEvent('touchstart', wrapEvent(e));
+    },
+    touchMove(e) {
+      const canvasEl = this.canvasEl;
+      if (!canvasEl) {
+        return;
+      }
+      canvasEl.dispatchEvent('touchmove', wrapEvent(e));
+    },
+    touchEnd(e) {
+      const canvasEl = this.canvasEl;
+      if (!canvasEl) {
+        return;
+      }
+      canvasEl.dispatchEvent('touchend', wrapEvent(e));
+    }
+  }
+});
